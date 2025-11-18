57a256a6759fefbf978905549695e66dfcd7b397
Junior de Oliveira Soares <1036603@senacminas.edu.br>
2025-11-18 17:23:54 -0300

    alterações

---
+++ desafios.js
@@ -0,0 +1,9 @@
+let playlist = ["Dua lipa - Dance the Night", "The weeknd - Lights"];
+
+playlist.push("djw 6");
+console.log(playlist);
+
+playlist.push("djw 7");
+console.log(playlist);
+
+let removido
