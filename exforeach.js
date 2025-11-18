84f57be32a8f65a0eca356a73c8033fb50aec1ce
Junior de Oliveira Soares <1036603@senacminas.edu.br>
2025-11-18 17:03:39 -0300

    alterações

---
+++ exforeach.js
@@ -0,0 +1,13 @@
+let gadmes = ["Minecraft", "Fortnite", "Roblox"];
+
+//mostrar na tela game
+gadmes.forEach((game, index) => {
+
+ console.log(`${index + 1}. ${game}`);   
+
+});
+
+//Resultado
+//1. Minecraft
+//2. Fortnite
+//3. Roblox
---
+++ map.js
@@ -0,0 +1,9 @@
+let numeros = [1,2,3,4,5];
+
+let dobrados = numeros.map((numero)=> {
+
+return numero * 2;
+
+});
+console.log(dobrados);
+ 
