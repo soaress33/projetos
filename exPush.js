1bf574626dca312d0b1abb8eaa3dee1bae1aa6f3
Junior de Oliveira Soares <1036603@senacminas.edu.br>
2025-11-18 16:17:36 -0300

    alterações

---
+++ exPush.js
@@ -0,0 +1,11 @@
+let jogos = ["Minecraft", "Fortnite"];
+
+//Adicionar um elemento final
+jogos.push("Roblox");
+console.log(jogos);
+
+jogos.push("Amog Us", "FIFA");
+console.log(jogos);
+
+
+
---
+++ expop.js
@@ -0,0 +1,6 @@
+let lanches = ["X-Burguer", "Pizza", "Sushi", "Açai"];
+
+//remover o ÚLTIMO elemento
+let removido = lanches.pop();
+console.log(removido);
+console.log(lanches); // ["X-Burguer", "Pizza", "Sushi"]
