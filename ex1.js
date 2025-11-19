dc5b21246142af93bc7a633e2e5ae260d85d2cc3
Junior de Oliveira Soares <1036603@senacminas.edu.br>
2025-11-19 14:55:05 -0300

    alterações

--- ex1.js
+++ ex1.js
@@ -2,26 +2,14 @@ let listas = []
 
 listas.push("uva ")
 listas.push("pera")
-
-
 listas.push("maça")
 console.log(listas)
-
 listas.push("jaca")
-
-
 listas.push("melancia")
-
 listas.push("peixe")
-
 listas.push("acerola")
-
-
 listas.push("banana")
-
 listas.push(" morango")
-
-
 listas.push("melao")
 console.log(listas)
 
---
+++ ex2.js
@@ -0,0 +1,9 @@
+let nomes= ["mazola", "fula", "pitoco"]
+
+nomes.push("juca")
+nomes.push("soares")
+console.log(nomes)
+
+nomes.pop(nomes)
+console.log(nomes);
+
