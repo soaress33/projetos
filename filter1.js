9508c0481e6d08e614f12b2bf865a52f257b8fe2
Junior de Oliveira Soares <1036603@senacminas.edu.br>
2025-11-19 17:22:02 -0300

    alterações

---
+++ filter1.js
@@ -0,0 +1,11 @@
+let nomes =  ["ana-15", "carlos-14", "maria-15", "joao-16", "beatriz-15", "Pedro-13", "Luiza-15", "Ricardo-14" ]; 
+
+let maior = nomes.filter((nome) =>{
+
+return nome.includes("-15")
+
+});
+
+
+console.log(maior);
+
---
+++ filter2.js
@@ -0,0 +1,3 @@
+let idades = [14, 15, 16, 15, 13, 15, 17, 15, 14, 15];
+
+let maior = idades
